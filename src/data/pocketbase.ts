3128cc5b0e12e2b7dbbed70fa374c01cc4eec12f
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-10-22 11:36:56 +0300

    added project pages

--- src/data/pocketbase.ts
+++ src/data/pocketbase.ts
@@ -4,7 +4,6 @@ export const pb = new PocketBase(
   import.meta.env.POCKETBASE_URL ||
     process.env.POCKETBASE_URL
 )
-
 export async function getProjects() {
   const projects = await pb
     .collection('projects')
