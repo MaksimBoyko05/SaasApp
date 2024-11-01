9fc6ffe56df4b63a9efe408c569a4e659e319eb9
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-11-01 13:23:40 +0200

    added fonts

--- src/data/pocketbase.ts
+++ src/data/pocketbase.ts
@@ -1,9 +1,13 @@
+import type {
+  TypedPocketBase,
+  ProjectsResponse,
+} from '@src/data/pocketbase-types'
 import PocketBase from 'pocketbase'
 
 export const pb = new PocketBase(
   import.meta.env.POCKETBASE_URL ||
     process.env.POCKETBASE_URL
-)
+) as TypedPocketBase
 pb.autoCancellation(false)
 
 export async function getProjects() {
@@ -11,7 +15,9 @@ export async function getProjects() {
     .collection('projects')
     .getFullList()
 
-  return projects
+  return projects.sort(
+    (a, b) => getStatus(a) - getStatus(b)
+  )
 }
 
 export async function addProject(name: string) {
@@ -55,3 +61,23 @@ console.log(options);
 console.log(tasks);
   return tasks
 }
+function getStatus(project: ProjectsResponse) {
+  switch (project.status) {
+    case "not started":
+      return 7;
+  case "on hold":
+    return 6;
+  case "started":
+    return 5;
+  case "in progress":
+    return 4;
+  case "almost finished":
+    return 3;
+  case "ongoing":
+    return 2;
+  case "done":
+   return 1;
+  default:
+    return 0;
+    }
+  }
